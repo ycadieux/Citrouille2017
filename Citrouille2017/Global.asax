3a8fef4438ff43fd619fae9d8dc05c9f5d64838b
ycadieux <ycadieux@telequebec.tv>
2017-10-27 15:42:17 -0400

    first commit

---
+++ Citrouille2017/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Citrouille2017.MvcApplication" Language="C#" %>
---
+++ Citrouille2017/Hubs/CitrouilleHub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace Citrouille2017.Hubs
+{
+    public class CitrouilleHub : Hub
+    {
+        public void Start()
+        {
+            Clients.All.start();
+        }
+    }
+}
